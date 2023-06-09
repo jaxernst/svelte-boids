db86e27cd692859dc6dbcb3e5fa6c6083c655f92
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-06-08 22:02:46 -0700

    Commit

--- src/lib/boid-engine/boid-creation.ts
+++ src/lib/boid-engine/boid-creation.ts
@@ -56,8 +56,8 @@ export const randomizeBoidType = (deviationFactor: number = 10) => {
 };
 
 function boxMullerRandom(mean = 0, stdev = 1, skew = 0) {
-  let u = 0,
-    v = 0;
+  let u = 0;
+  let v = 0;
   while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
   while (v === 0) v = Math.random();
   let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
