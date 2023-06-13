8cf10397fe2c934d828091d491ed595b2a7bae85
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-06-12 23:13:51 -0700

    Add tailwing support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      rounded: {
+        lg: "8px",
+      },
+    },
+  },
+  plugins: [],
+};
